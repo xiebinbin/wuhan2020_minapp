cb81ee188f3c13fc5eb63a6a0d26429243e9cca1
Jeff Cui <xc1008@nyu.edu>
2020-02-03 16:24:40 -0500

    update colletion page

--- src/pages/favorite/index.tsx
+++ src/pages/favorite/index.tsx
@@ -1,19 +1,55 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 
 import './index.scss'
+import mockData from "./mockData"
 
 class Index extends Component {
-    config: Config = {
-    navigationBarTitleText: '我的'
+
+  config: Config = {
+        navigationBarTitleText: '收藏'
+  }
+
+  state = {
+    data: mockData
+  }
+
+  toDetailPage = (id) => {
+    console.log(id)
   }
 
   render () {
     return (
-      <View className='index'>
-        <View><Text>收藏</Text></View>
+      <View className='at-row at-row--wrap favorite-containter'>
+        {
+          this.state.data.map(item => {
+            const {id, imgUrl, title, description, time, type} = item
+            return <View 
+                    onClick={() => {this.toDetailPage(id)}}
+                    className='at-col at-col-6 at-col--wrap favorite-block-padding' 
+                    key={id}>
+                        <View style="at-article">
+                         <Image 
+                            className="favorite-block-img"
+                            src={imgUrl}
+                          />
+                          <View className="at-article__h3">
+                            <Text>{title}</Text>
+                          </View>
+                          <View className="at-article__info favorite-block-description">
+                            <Text>{description}</Text>
+                          </View>
+                          <View className='at-row at-row__justify--between'>
+                            <View className='at-col at-col-2'>{time}</View>
+                            <View className='at-col at-col-2'>{type}</View>
+                          </View>
+                        </View>
+                   </View>
+          })
+        }
       </View>
     )
   }
 }
+
 export default Index;
---
+++ src/pages/favorite/mockData.tsx
@@ -0,0 +1,46 @@
+import PlaceHolderImage from "../../../assets/placeHolderImg.png"
+
+const mockData = [
+    {
+      id:1,
+      imgUrl: PlaceHolderImage,
+      title: "哈哈哈",
+      description: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+      time: "now",
+      type: "car"
+    },
+    {
+      id:2,
+      imgUrl: PlaceHolderImage,
+      title: "哈哈哈",
+      description: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+      time: "now",
+      type: "car"
+    },
+    {
+      id:3,
+      imgUrl: PlaceHolderImage,
+      title: "哈哈哈",
+      description: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+      time: "now",
+      type: "car"
+    },
+    {
+      id:4,
+      imgUrl: PlaceHolderImage,
+      title: "哈哈哈",
+      description: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+      time: "now",
+      type: "car"
+    },
+    {
+      id:5,
+      imgUrl: PlaceHolderImage,
+      title: "哈哈哈",
+      description: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+      time: "now",
+      type: "car"
+    }
+  ]
+
+export default mockData
