6a5921a3f680c0d11f6c6c86c70108f27ed25b15
Jeff Cui <xc1008@nyu.edu>
2020-02-04 00:05:58 -0500

    update index home index page

--- src/pages/home/index/index.tsx
+++ src/pages/home/index/index.tsx
@@ -1,6 +1,8 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtList, AtListItem, AtAvatar, AtButton } from "taro-ui"
 
+import mockData from "./mockData"
 import './index.scss'
 
 class Index extends Component {
@@ -8,12 +10,49 @@ class Index extends Component {
     navigationBarTitleText: '我的'
   }
 
+  state = {
+    data: mockData
+  }
+
+  handleClickInfo = () => {
+    console.log("Clicked")
+  }
+
+  handleLogout = () => {
+    console.log("logout")
+  }
+
   render () {
+    const {imgUrl, name, job, description} = this.state.data
     return (
-      <View className='index'>
-        <View><Text>我的</Text></View>
+      <View className="profile-containter">
+        <View className='at-row profile-header'>
+          <View className='at-col-4 profile-header-imgView'>
+            <AtAvatar size="large" circle image={imgUrl} />
+          </View>
+          <View className='at-col-8 at-article'>
+            <View className="at-article__h3">
+              <Text>{name}</Text>
+            </View>
+            <View className="at-article__p">
+              <Text>{job}</Text>
+            </View>
+            <View className="at-article__p">
+              <Text>{description}</Text>
+            </View>
+          </View>
+        </View>
+        <AtList hasBorder={false}>
+          <AtListItem title='我的发布' arrow='right' onClick={this.handleClickInfo} />
+          <AtListItem title='编辑资料' arrow='right' onClick={this.handleClickInfo}/>
+          <AtListItem title='关于我们' arrow='right' onClick={this.handleClickInfo}/>
+        </AtList>
+        <View className="profile-logout-button">
+         <AtButton type='primary' onClick={this.handleLogout}>退出登录</AtButton>
+        </View>
       </View>
     )
   }
 }
+
 export default Index;
---
+++ src/pages/home/index/mockData.tsx
@@ -0,0 +1,10 @@
+import Img from "../../../../assets/placeHolderImg.png"
+
+const data = {
+    imgUrl: Img,
+    name: "小伙子",
+    job: "哈哈",
+    description: "哈哈哈哈"
+}
+
+export default data
