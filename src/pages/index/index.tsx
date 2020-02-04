da9d0fd9ac6d3392c3042cad479a9083e81c7573
Shuqi Liao <373628977@qq.com>
2020-02-04 21:46:59 +0800

    update layout of index page

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,15 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
+import {Image, View} from '@tarojs/components'
+import {AtSearchBar, AtTabs, AtTabsPane} from "taro-ui";
 import { connect } from '@tarojs/redux'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.scss'
 import { queryAllCate } from '../../graphql/query/cate/index'
+import mockSuppliers from "./mockSupplierData"
+
 // #region 书写注意
 //
 // 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
@@ -65,6 +68,13 @@ class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
+  state = {
+      value: '',
+      current: 0,
+      statusBarHeight: '',
+      supplierList: mockSuppliers,
+  }
+
   componentWillReceiveProps (nextProps) {
     console.log(this.props, nextProps)
   }
@@ -72,6 +82,14 @@ class Index extends Component {
     queryAllCate().then((data) => {
       console.log(data)
     })
+    Taro.getSystemInfo({
+      success: res => {
+        console.log(res.statusBarHeight);
+        this.setState({
+          statusBarHeight: res.statusBarHeight + 'px'
+        })
+      }
+    }).then(res => console.log(res))
   }
   componentWillUnmount () {
   }
@@ -80,19 +98,88 @@ class Index extends Component {
 
   componentDidHide () { }
 
+  onChange (value) {
+    this.setState({
+      value: value
+    })
+    return value
+  }
+
+  handleClick (value) {
+    this.setState({
+      current: value
+    })
+  }
+
+  renderSuppliers () {
+      const { supplierList } = this.state
+
+      let renderList = supplierList.map( item =>
+          <View className='index-item'>
+            <View className='index-item-head'>
+              <View className='item-head-left'>
+                <Image
+                  className='index-item-avatar'
+                  src={item.userAvatar}
+                />
+                <View className='index-item-head-title'>
+                  <View className='head-title-name'>{item.userName}</View>
+                  <View className='head-title-data'>{item.uploadDate}</View>
+                </View>
+              </View>
+              <View className='index-item-head-type'>{item.type}</View>
+            </View>
+            <View className='index-item-content'>
+              {item.message}
+            </View >
+            <View className='item-images'>
+              {
+                item.images.map ( image =>
+                  <Image
+                    className='index-item-image'
+                    src={image}
+                  />
+                )
+              }
+            </View>
+          </View>
+      )
+      return <View className='index-list'>{renderList}</View>;
+  }
+
+
+
   render () {
+    const tabList = [{ title: '需求' }, { title: '援助' }]
     return (
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <Button className='dec_btn' onClick={() => {
-          Taro.navigateTo({
-            url: '/pages/about/index'
-          })
-        }}>关于我们</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>首页</Text></View>
+        {/*<Button className='add_btn' onClick={this.props.add}>+</Button>*/}
+        {/*<Button className='dec_btn' onClick={this.props.dec}>-</Button>*/}
+        {/*<Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>*/}
+        {/*<Button className='dec_btn' onClick={() => {*/}
+        {/*  Taro.navigateTo({*/}
+        {/*    url: '/pages/about/index'*/}
+        {/*  })*/}
+        {/*}}>关于我们</Button>*/}
+        {/*<View><Text>{this.props.counter.num}</Text></View>*/}
+        {/*<View><Text>首页</Text></View>*/}
+        <View className='status-bar' style={{ height: this.state.statusBarHeight}}></View>
+        <View className='index-content'>
+          <View className='filter-search-bar'>
+            <AtSearchBar
+              value={this.state.value}
+              onChange={this.onChange.bind(this)}
+            />
+          </View>
+          <AtTabs current={this.state.current} tabList={tabList} onClick={this.handleClick.bind(this)}>
+            <AtTabsPane current={this.state.current} index={0} >
+              {this.renderSuppliers()}
+            </AtTabsPane>
+            <AtTabsPane current={this.state.current} index={1}>
+              <View style='height:100%;padding: 100px 50px;background-color: #FAFBFC;text-align: center;'>标签页二的内容</View>
+            </AtTabsPane>
+          </AtTabs>
+        </View>
       </View>
     )
   }
---
+++ src/pages/index/mockSupplierData.tsx
@@ -0,0 +1,57 @@
+import avatar from "../../../assets/default_avator.png";
+import defaultImage from "../../../assets/default_image.png";
+
+const suppliers = [{
+  userAvatar: avatar,
+  userName: "武汉协和医院",
+  uploadDate: "2020-02-02 14:23:45",
+  type: "医院",
+  message: "求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙",
+  images: [
+    defaultImage,
+    defaultImage,
+    defaultImage,
+    defaultImage,
+  ]
+  }, {
+  userAvatar: avatar,
+  userName: "热心市民刘大姐",
+  uploadDate: "2020-02-02 14:23:45",
+  type: "个人",
+  message: "求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙",
+  images: [
+    defaultImage,
+    defaultImage,
+    defaultImage,
+    defaultImage,
+  ]
+  }, {
+  userAvatar: avatar,
+  userName: "武汉协和医院",
+  uploadDate: "2020-02-02 14:23:45",
+  type: "医院",
+  message: "求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙",
+  images: [
+    defaultImage,
+    defaultImage,
+    defaultImage,
+    defaultImage,
+  ]
+  }, {
+  userAvatar: avatar,
+  userName: "某某热心人士",
+  uploadDate: "2020-02-02 14:23:45",
+  type: "个人",
+  message: "求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙求求大家帮帮忙",
+  images: [
+    defaultImage,
+    defaultImage,
+    defaultImage,
+    defaultImage,
+  ]
+  }
+]
+
+export default suppliers
+
+
